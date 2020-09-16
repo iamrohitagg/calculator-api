5f2bebe3a06c0f4ce74d365ef24050d98be45800
imarohitagg <rohitaggarwal376@gmail.com>
2020-09-16 18:33:37 +0530

    change routes

--- src/index.js
+++ src/index.js
@@ -26,15 +26,10 @@ const checkNumber = (req, res) => {
   let num1 = req.body.num1;
   let num2 = req.body.num2;
   if (typeof num1 === "string" || typeof num2 === "string") {
-    res.send(
-      JSON.stringify({
-        status: "error",
-        message: "invalid data types",
-      })
-    );
-    // If we don't return a value then we cannnot send after being sent to the client
-    // Because of the asynchronous behaviour of node js
-    return true;
+    return res.json({
+      status: "error",
+      message: "invalid data types",
+    });
   }
 };
 
@@ -42,13 +37,10 @@ const checkNumber = (req, res) => {
 // Must be less than 10M
 const checkInputLimit = (num1, num2, res) => {
   if (num1 > inputlimit || num2 > inputlimit) {
-    res.send(
-      JSON.stringify({
-        status: "error",
-        message: "overflow",
-      })
-    );
-    return true;
+    return res.json({
+      status: "error",
+      message: "overflow",
+    });
   }
 };
 
@@ -57,97 +49,81 @@ app.post("/add", (req, res) => {
   let num1 = req.body.num1;
   let num2 = req.body.num2;
 
-  if (checkNumber(req, res)) {
-    return;
-  }
-  if (checkInputLimit(num1, num2, res)) {
-    return;
+  checkNumber(req, res);
+  checkInputLimit(num1, num2, res);
+
+  if (num1 + num2 > inputlimit) {
+    return res.json({
+      status: "error",
+      message: "Overflow",
+    });
   }
 
-  res.send(
-    JSON.stringify({
-      status: "success",
-      message: "the sum of given two number",
-      sum: num1 + num2,
-    })
-  );
+  return res.json({
+    status: "success",
+    message: "the sum of given two number",
+    sum: num1 + num2,
+  });
 });
 
 // API route for subtracting numbers
 app.post("/sub", (req, res) => {
   let num1 = req.body.num1;
   let num2 = req.body.num2;
-  if (checkNumber(req, res)) {
-    return;
-  }
 
-  if (checkInputLimit(num1, num2, res)) {
-    return;
-  }
+  checkNumber(req, res);
+  checkInputLimit(num1, num2, res);
 
   if (num1 - num2 < -1000000) {
-    res.send(
-      JSON.stringify({
-        status: "error",
-        message: "Underflow",
-      })
-    );
+    res.json({
+      status: "error",
+      message: "Underflow",
+    });
   }
-  res.send(
-    JSON.stringify({
-      status: 200,
-      message: "the difference of given two number",
-      sum: num1 - num2,
-    })
-  );
+
+  res.json({
+    status: 200,
+    message: "the difference of given two number",
+    sum: num1 - num2,
+  });
 });
 
 // API route to multiply number
 app.post("/multiply", (req, res) => {
   let num1 = req.body.num1;
   let num2 = req.body.num2;
-  if (checkNumber(req, res)) {
-    return;
-  }
 
-  if (checkInputLimit(num1, num2, res)) {
-    return;
-  }
-  res.send(
-    JSON.stringify({
-      status: "success",
-      message: "The product of given numbers",
-      sum: num1 * num2,
-    })
-  );
+  checkNumber(req, res);
+
+  checkInputLimit(num1, num2, res);
+
+  res.json({
+    status: "success",
+    message: "The product of given numbers",
+    sum: num1 * num2,
+  });
 });
 
 // API route to divide number
 app.post("/division", (req, res) => {
   let num1 = req.body.num1;
   let num2 = req.body.num2;
-  if (checkNumber(req, res)) {
-    return;
+
+  checkNumber(req, res);
+  checkInputLimit(num1, num2, res);
+
+  if (num2 === 0) {
+    return res.json({
+      status: "error",
+      message: "Cannot divide by zero",
+    });
   }
 
-  if (checkInputLimit(num1, num2, res)) {
-    return;
-  }
-  if (num2 === 0) {
-    res.send(
-      JSON.stringify({
-        status: "error",
-        message: "Cannot divide by zero",
-      })
-    );
-  }
-  res.send(
-    JSON.stringify({
-      status: "success",
-      message: "The division of given numbers",
-      sum: num1 / num2,
-    })
-  );
+  res.json({
+    status: "success",
+    message: "The division of given numbers",
+    sum: num1 / num2,
+  });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
